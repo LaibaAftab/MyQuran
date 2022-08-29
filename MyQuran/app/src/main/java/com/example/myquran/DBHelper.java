c3b4a4cae83766e7398b66805f8a71225ce89c94
LaibaaAftab <laiba.aftab@hotelkeyapp.com>
2022-08-29 22:51:24 +0500

    updated app

---
+++ MyQuran/app/src/main/java/com/example/myquran/DBHelper.java
@@ -0,0 +1,2 @@
+package com.example.myquran;public class DBHelper {
+}
---
+++ MyQuran/app/src/main/java/com/example/myquran/searchayaat.java
@@ -0,0 +1,14 @@
+package com.example.myquran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class searchayaat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchayaat);
+    }
+}
